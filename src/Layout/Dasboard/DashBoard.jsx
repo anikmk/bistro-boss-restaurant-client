f71cb091e88346b0d8bc2f09554bfb9bf7bfc2f3
anikmk <anikmollik122321@gmail.com>
2023-11-19 15:37:47 +0600

    delete operation with axios

--- src/Layout/Dasboard/DashBoard.jsx
+++ src/Layout/Dasboard/DashBoard.jsx
@@ -1,8 +1,10 @@
-import { FaAd, FaCalendar, FaHome, FaShoppingCart } from "react-icons/fa";
+import { FaAd, FaCalendar, FaHome, FaSearch, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../../hooks/useCart";
 
 
 const DashBoard = () => {
+    const [cart] = useCart();
     return (
         <div className="flex">
             <div className="w-64 min-h-full bg-orange-400">
@@ -19,7 +21,7 @@ const DashBoard = () => {
                     </li>
                     <li>
                     <NavLink to='/dashboard/cart'> <FaShoppingCart></FaShoppingCart>
-                    My Cart
+                    My Cart ({cart.length})
                     </NavLink>
                     </li>
                     <li>
@@ -32,9 +34,21 @@ const DashBoard = () => {
                     Bookings
                     </NavLink>
                     </li>
+                    {/* divider */}
+                    <div className="divider"></div>
+                    <li>
+                    <NavLink to='/'> <FaHome></FaHome>
+                     Home
+                    </NavLink>
+                    </li>
+                    <li>
+                    <NavLink to='/order/salad'> <FaSearch></FaSearch>
+                     Menu
+                    </NavLink>
+                    </li>
                 </ul>
             </div>
-            <div className="flex-1">
+            <div className="flex-1 p-7">
                 <Outlet></Outlet>
             </div>
         </div>
