efbab2729ef57901c83e397a62a9ae2c61755b79
anikmk <anikmollik122321@gmail.com>
2023-11-21 20:55:41 +0600

    get token error status code

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,6 +4,22 @@ import axios from "axios";
     baseURL:'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    axiosSecure.interceptors.request.use(function(config){
+        const token = localStorage.getItem('access-token')
+        console.log('request stped by intercepters',token)
+        config.headers.authorization = `Bearer ${token}`;
+        return config;
+    },function (error){
+        return Promise.reject(error);
+    })
+    // intercepter 401 and 403
+    axiosSecure.interceptors.response.use(function(response){
+        return response;
+    },(error)=>{
+        const status = error.response.status
+        console.log('status code in the interceptor',status)
+        return Promise.reject(error)
+    })
     return axiosSecure;
 };
 
